9ae3e16e9999379b52c94bc9da9b291839a74aec
Matthew Rahtz <matthew.rahtz@gmail.com>
2017-01-04 16:45:54 +0100

    Animation of the two different PCs

--- pca_test.py
+++ pca_test.py
@@ -3,7 +3,9 @@ import numpy as np
 import wave
 import matplotlib.pyplot as plt
 import struct
+import time
 from sklearn.decomposition import  PCA
+from matplotlib.animation import FuncAnimation
 
 volume = 1     # range [0.0, 1.0]
 fs = 44100       # sampling rate, Hz, must be integer
@@ -29,11 +31,21 @@ print(len(windows))
 pca = PCA(n_components=2)
 pca.fit(windows)
 transformed_windows = pca.transform(windows)
-plt.scatter(transformed_windows[:, 0], transformed_windows[:, 1])
-plt.show()
 
-plt.plot(samples)
-#plt.show()
+fig = plt.figure()
+plt.xlim([-10, 10])
+plt.ylim([-10, 10])
+scat = plt.scatter(transformed_windows[0, 0], transformed_windows[0, 1])
+i = 0
+def update(frame_n):
+    t = int(frame_n / 30 * 44100)
+    t_start = t - 100
+    if t_start < 0:
+        t_start = 0
+    scat.set_offsets(transformed_windows[t_start:t, :])
+
+animation = FuncAnimation(fig, update, interval=int(1000/30))
+plt.show()
 
 # samples = [int(32767 * x) for x in samples]
 # sample_words = \
