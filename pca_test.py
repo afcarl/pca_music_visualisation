85ac2b99d90c89ae81121f23b3169d20e2f9cd72
Matthew Rahtz <matthew.rahtz@gmail.com>
2017-01-04 16:00:56 +0100

    PCA of two sine tones, windows strided one window length apart. It gives two ovals!

---
+++ pca_test.py
@@ -0,0 +1,47 @@
+from __future__ import print_function, division
+import numpy as np
+import wave
+import matplotlib.pyplot as plt
+import struct
+from sklearn.decomposition import  PCA
+
+volume = 1     # range [0.0, 1.0]
+fs = 44100       # sampling rate, Hz, must be integer
+duration = 1.0   # in seconds, may be float
+f = 440.0        # sine frequency, Hz, may be float
+
+samples = []
+for f in [220, 440, 220, 440, 220, 440, 220, 440]:
+    # generate samples, note conversion to float32 array
+    samples1 = (np.sin(2*np.pi*np.arange(fs*duration)*f/fs)).astype(np.float32)
+    samples.extend(samples1)
+print(len(samples))
+
+window_len_samples = 128
+windows = np.zeros(window_len_samples)
+start = 0
+end = window_len_samples
+stride = window_len_samples
+while end < len(samples):
+    windows = np.vstack([windows, [samples[start:end]]])
+    start += stride
+    end += stride
+
+print(len(windows))
+pca = PCA(n_components=2)
+pca.fit(windows)
+transformed_windows = pca.transform(windows)
+plt.scatter(transformed_windows[:, 0], transformed_windows[:, 1])
+plt.show()
+
+plt.plot(samples)
+#plt.show()
+
+# samples = [int(32767 * x) for x in samples]
+# sample_words = \
+#     struct.pack('%dh' % len(samples), *samples)
+# w = wave.open('/tmp/foo.wav', 'wb')
+# w.setsampwidth(2)
+# w.setnchannels(1)
+# w.setframerate(44100)
+# w.writeframes(sample_words)
